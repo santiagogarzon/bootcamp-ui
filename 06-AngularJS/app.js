b9309a03f244fa023b18a210e1d88d70ca35c743
SANTIAGO <garzon02@hotmail.com>
2015-11-16 13:53:39 -0300

    Final correction

--- 06-AngularJS/app.js
+++ 06-AngularJS/app.js
@@ -1,7 +1,55 @@
 var app= angular.module('angulapp', []);
 
+app.value("movies" , [ {
+		title: "Terminator",
+		type: "Action",
+		year: "2002",
+		source: "netflix",
+		score: 8,
+		url: "http://t1.gstatic.com/images?q=tbn:ANd9GcRHzSaUCOKu1RwS-bfbaUeeo_I1JcBkiuJRjBElvJi7qsHXkUkJ",
+		id: 0
+		},
+	{
+		title: "Matrix",
+		type: "Action",
+		year: "2005",
+		source: "netflix",
+		score: 7,
+		url: "http://t0.gstatic.com/images?q=tbn:ANd9GcQq3pIz-aKgkmYX1dJ-EL-AlHSPcOO7wdqRIJ5gJy9qNinXpmle",
+		id: 1
+	},
+	{
+		title: "Iron Man",
+		type: "Action",
+		year: "2009",
+		source: "Direct Tv",
+		score: 6,
+		url: "http://t2.gstatic.com/images?q=tbn:ANd9GcQD3vPP1cn4hWapVXW7y8DE5oDs3IRKizlga-5FaE9vMqTnSSzgG70roQ",
+		id: 2
+	},
+	{
+		title: "Whiplash",
+		type: "Drama",
+		year: "2014",
+		source: "PayPerView",
+		score: 9,
+		url: "http://t3.gstatic.com/images?q=tbn:ANd9GcS_IwW5-_mWA1PXiPG4qEhLC6Q3vntQd7Bzgs_YE7HHFifItn2T",
+		id: 3
+	},
+	{
+		title: "Robocop",
+		type: "action",
+		year: "1987",
+		source: "netflix",
+		score: 8,
+		url: "http://www.gstatic.com/tv/thumb/movieposters/10144/p10144_p_v7_ab.jpg",
+		id: 4
+	}
 
-app.controller('moviesCtrl', function($scope, movieService) {
+
+	]);
+
+app.controller('moviesCtrl',["$scope" ,"movies" ,"movieService", function($scope, movies, movieService) {
 	$scope.movies= movies;
 	$scope.selected= $scope.movies[0];
 	$scope.hide= false;
@@ -29,21 +77,21 @@ app.controller('moviesCtrl', function($scope, movieService) {
 
 	};
 	$scope.new= function() {
+		$scope.newmovie={};
 		$scope.hide= true;
 		$scope.hide2= true;
 		$scope.hide3= false;
 	};
 	$scope.delete = function(id){
-		movieService.delete(id);
 		$scope.newmovie={};
 		$scope.back();
-
+		return (movieService.delete(id));
 	};
 
 	
-});
+}]);
 
-app.service("movieService", function(){
+app.service("movieService",[ "movies", function(movies){
 	var uid=5;
 	
 	this.save= function (movie) {
@@ -57,7 +105,7 @@ app.service("movieService", function(){
 				}
 			}
 		}
-	}
+	};
 
 	this.get = function(id) {
 		for(i in movies){
@@ -65,14 +113,14 @@ app.service("movieService", function(){
 				return movies[i];
 			}
 		}
-	}
+	};
 
 	this.delete= function(id) {
 		for(i in movies){
-			if(movies[i].id=id){
-				movies.splice(i+1, 1);
+			if(movies[i].id==id){
+				movies.splice(i, 1);
 			}
 		}
-	}
-});
+	};
+}]);
 
